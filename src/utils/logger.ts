e173585511290f36a4a2f59ce12c6f8b9962a548
Murat <muratx10@gmail.com>
2021-06-05 23:32:15 +0300

    refactor: clean up code

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -4,12 +4,16 @@ const logger = (msg: string, level = 'common'): void => {
   const filename = `${level}.txt`;
   const message = `Date: ${new Date().toISOString()}\n${msg}\n`;
 
-  appendFile(filename, message, (err: NodeJS.ErrnoException | null): void => {
-    if (err) {
-      // eslint-disable-next-line no-console
-      console.error(`Failed to save log [${message}] to file [${level}]: Error: ${err}`);
-    }
-  });
+  appendFile(
+    filename,
+    message,
+    (err: NodeJS.ErrnoException | null): void => {
+      if (err) {
+        // eslint-disable-next-line no-console
+        console.error(`Failed to save log [${message}] to file [${level}]: ` +
+          `Error: ${err}`);
+      }
+    });
 };
 
 export default logger;
