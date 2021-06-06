380e9ac6414e17f9fda711c223756af4f6f03553
Murat <muratx10@gmail.com>
2021-06-06 22:06:59 +0300

    refactor: add console for error handlers

--- src/utils/errHandlers.ts
+++ src/utils/errHandlers.ts
@@ -1,13 +1,18 @@
+/* eslint-disable no-console */
 import logger from './logger';
 
 export const exceptionHandler = (err: Error, origin: string): void => {
   const message = `${origin}: ${err.toString()}`;
+
+  console.error(message);
 
   logger(message, 'error');
 };
 
 export const rejectionHandler = (reason: Error): void => {
   const message = `Unhandled rejection error: ${reason}`;
+
+  console.error(message);
 
   logger(message, 'error');
 };
